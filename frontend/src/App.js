e3cb7514c309ec375ffd341d00577330e64dbe23
Nagapurnasaiguntu <124567535+NagaPurnaSai@users.noreply.github.com>
2025-04-17 14:21:14 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Customer from "./Customer";
+import AdminLogin from "./AdminLogin";
 import Admin from "./Admin";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/customer" element={<Customer />} />
+        <Route path="/admin-login" element={<AdminLogin />} /> 
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
